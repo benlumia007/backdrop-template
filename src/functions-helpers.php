f64c9c011361db400bcf8ecdaa1c9a6658bf0b85
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-01-22 14:11:26 -0800

    change get_stylesheet_directory to get_parent_theme_file_path()

--- src/functions-helpers.php
+++ src/functions-helpers.php
@@ -78,7 +78,7 @@ if ( ! function_exists( __NAMESPACE__ . '\\locations' ) ) {
 		$path = ltrim( path(), '/' );
 
 		// Add active theme path.
-		$locations = [ get_stylesheet_directory() . "/{$path}" ];
+		$locations = [ get_parent_theme_file_path( path() ) ];
 
 		// If child theme, add parent theme path second.
 		if ( is_child_theme() ) {
